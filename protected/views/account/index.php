8072f4ddcbf4d7d19d6a5b4ddea4dc39162a40ec
Andrew <Andrew@Andrew-Laptop>
2012-07-21 21:16:38 -0400

    Modified 7/21/12

--- protected/views/account/index.php
+++ protected/views/account/index.php
@@ -5,7 +5,7 @@
     <div id="content-container">
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Congratulations! You have successfully created your Yii application. MODIFIED BY ANDREW</p>
 
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
