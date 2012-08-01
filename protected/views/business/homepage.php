d7308ade5126cfc3b89b34044dd933917107b45b
Andrew <Andrew@Andrew-Laptop>
2012-07-31 22:37:07 -0400

    Modified Business home page and create Competition form. 7/21/12

--- protected/views/business/homepage.php
+++ protected/views/business/homepage.php
@@ -1,3 +1,11 @@
 <?php
     echo $model -> name;
+    foreach($model->competitions as $competition){
+        
+        echo $competition -> id . '</br>';
+        echo $competition -> name . '</br>';
+        echo $competition -> description . '</br>';
+        echo $competition -> startDate . '</br>';
+        echo $competition -> endDate . '</br>';
+    }
 ?>
