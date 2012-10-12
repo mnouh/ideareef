8a884a02ee83048406822082154d2082bc5d9df0
Mohamed Nouh <mnouh3@gmail.com>
2012-10-12 17:30:21 -0400

    Git Update Script.

--- protected/config/main.php
+++ protected/config/main.php
@@ -82,7 +82,6 @@ return array(
                                 array('api/competitions', 'pattern' => 'api/<model:\w+>', 'verb' => 'POST'),
                                 '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
                                 'aboutus' => 'account/aboutus',
-                                'update' => 'account/update',
                                 'companies' => 'account/business',
                                 'competition' => 'competition/index',
                                 'login' => 'account/login',
