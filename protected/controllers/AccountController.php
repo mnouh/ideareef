8b4efcbede626cdc89619ac3adaec59a542b092a
Mohamed Nouh <mnouh3@gmail.com>
2012-10-13 00:05:32 -0400

    Testing Cron Job

--- protected/controllers/AccountController.php
+++ protected/controllers/AccountController.php
@@ -29,6 +29,7 @@ class AccountController extends Controller
 	 */
 	public function actionIndex()
 	{
+            echo "AGAIN AGAIN";
             $this->layout = 'index';
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
